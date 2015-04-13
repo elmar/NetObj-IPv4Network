d3d05f51543b4b02ea2a265dbbc674b467d99377
Elmar S. Heeb <elmar@heebs.ch>
2015-04-13 15:14:07 +0200

    creating an object of class NetObj::IPv4Network

--- lib/NetObj/IPv4Network.pm
+++ lib/NetObj/IPv4Network.pm
@@ -5,4 +5,21 @@ package NetObj::IPv4Network;
 
 # ABSTRACT: represent an IPv4 network
 
+use Moo;
+
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+  use NetObj::IPv4Network;
+
+  # constructor
+  my $net1 = NetObj::IPv4Network->new('192.168.0.0/16');
+
+=head1 DESCRIPTION
+
+NetObj::IPv4Network represents IPv4 networks (subnets).
+
+NetObj::IPv4Network is implemented as a Moose style object class (using Moo).
--- t/100-use.t
+++ t/100-use.t
@@ -9,3 +9,9 @@ use lib '../lib';
 use Test::More; END { done_testing; }
 
 BEGIN { use_ok('NetObj::IPv4Network'); }
+
+is(
+    ref(NetObj::IPv4Network->new('192.168.0.0/16')),
+    'NetObj::IPv4Network',
+    'NetObj::IPv4Network must be a class',
+);
